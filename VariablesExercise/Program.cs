254dcabb5f32dc18993dddb78e3d6e958bc15771
b-juu <erichassett4@gmail.com>
2023-09-28 11:03:59 -0400

    initial commit

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -5,6 +5,13 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            string myName = "Eric Hassett";
+            int myAge = 27;
+            char myGrade = 'A';
+            bool isAlive = true;
+            double myBank = 9999.99;
+            decimal myBills = 10000.00m;
+            Console.WriteLine($"My name is {myName}, I am {myAge} years old");
         }
     }
 }
